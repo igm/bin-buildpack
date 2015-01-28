6352faa6f9656d51ffb5a951e7f319bcdc1cb6ad
Igor Mihalik <igor.mihalik@gmail.com>
2015-01-28 16:58:10 +0000

    update

--- src/agent/main.go
+++ src/agent/main.go
@@ -3,19 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"os/exec"
 )
 
 func main() {
+	go func() {
+		cmd := exec.Command("./server")
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println(err)
+		}
+		cmd.Wait()
+	}()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	fmt.Println("Listening on port: ", port)
-	http.HandleFunc("/", handlerHW)
-	err := http.ListenAndServe(":"+port, nil)
-	log.Fatal(err)
+	u, err := url.Parse("http://localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rp := httputil.NewSingleHostReverseProxy(u)
+
+	tr := &http.Transport{
+		Dial: func(network, addr string) (net.Conn, error) {
+			return net.Dial("unix", "/tmp/server.sock")
+		},
+	}
+	rp.Transport = tr
+	http.Handle("/", rp)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func handlerHW(rw http.ResponseWriter, req *http.Request) {
